11a3d038a578a605f7460f2508371e3630cb3171
mecctro <mecctro@users.noreply.github.com>
2015-02-01 22:26:10 -0500

    Update cjdns-install.sh

--- cjdns-install.sh
+++ cjdns-install.sh
@@ -35,6 +35,7 @@ echo 'export PATH=$PATH:$HOME/projects/go/bin' >> ~/.bashrc
 echo 'Install cjdcmd-ng'
 apt-get install go -y
 go get github.com/ehmry/cjdcmd-ng
+mv ./projects/go/bin/cjdcmd-ng ./projects/go/bin/cjdcmd
 
 # Generate config / clean config json
 echo 'Generate clean JSON config'
